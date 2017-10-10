e37d59a74a47a11f83bd7a4c9bc847381e4aa1d4
s3598797 <s3598797@student.rmit.edu.au>
2017-10-11 03:01:52 +1100

    fix error with missing ','

--- scripts/userManagement.pp
+++ scripts/userManagement.pp
@@ -15,21 +15,21 @@ class userManagement {
   user { 'becca':
     ensure => present,
     home => '/home/becca',
-    uid =>  '10018797'
+    uid =>  '10018797',
     shell => '/bin/bash',
     groups => ['sysadmin','cars'],
   }
   user { 'fred':
     ensure => present,
     home => '/home/fred',
-    uid =>  '10028797'
+    uid =>  '10028797',
     shell => '/bin/csh',
     groups => ['trucks','cars'],
   }
   user { 'wilma':
     ensure => present,
     home => '/home/wilma',
-    uid =>  '10028797'
+    uid =>  '10028797',
     groups => ['trucks','cars','ambulances'],
   }
   
