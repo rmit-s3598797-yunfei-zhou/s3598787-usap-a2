761c6cb4dd25ba7d9e46f1be0485cdaf2e12afa1
s3598797 <s3598797@student.rmit.edu.au>
2017-10-11 18:01:14 +1100

    package update for /usr/bin/wget

--- scripts/packagesManagement.pp
+++ scripts/packagesManagement.pp
@@ -1,7 +1,7 @@
 class packagesManagement {
 
   exec{ 'enableOptional':
-    command => 'sudo yum-config-manager --enable rhui-REGION-rhel-server-extras rhui-REGION-rhel-server-optional'
+    command => 'sudo /usr/bin/yum-config-manager --enable rhui-REGION-rhel-server-extras rhui-REGION-rhel-server-optional'
   }#vYou need to enable the optional channels. On AWS this is done in a different way: yum-config-manager --enable rhui-REGION-rhel-server-extras rhui-REGION-rhel-server-optional
 #https://serverfault.com/questions/725272/how-to-install-lynx-on-an-aws-redhat-machine-that-cant-find-the-package
 
@@ -68,11 +68,11 @@ class packagesManagement {
 
   exec { 'install_dia2code':  # exec resource named 
  
-    command => 'wget https://jaist.dl.sourceforge.net/project/dia2code/dia2code/0.8.3/dia2code-0.8.3-3.1.i586.rpm && sudo rpm -Uvh dia2code-0.8.3-3.1.i586.rpm && rm dia2code-0.8.3-3.1.i586.rpm',  # command this resource will run
+    command => '/usr/bin/wget  https://jaist.dl.sourceforge.net/project/dia2code/dia2code/0.8.3/dia2code-0.8.3-3.1.i586.rpm && sudo rpm -Uvh dia2code-0.8.3-3.1.i586.rpm && rm dia2code-0.8.3-3.1.i586.rpm',  # command this resource will run
   }
 
   exec { 'install_mysql':  # exec resource named 
-    command => 'wget https://dev.mysql.com/get/mysql57-community-release-el7-11.noarch.rpm && sudo rpm -Uvh mysql57-community-release-el7-11.noarch.rpm && rm mysql57-community-release-el7-11.noarch.rpm',  # command this resource will run
+    command => '/usr/bin/wget https://dev.mysql.com/get/mysql57-community-release-el7-11.noarch.rpm && sudo rpm -Uvh mysql57-community-release-el7-11.noarch.rpm && rm mysql57-community-release-el7-11.noarch.rpm',  # command this resource will run
   }
   
   package { 'mysql-server':
