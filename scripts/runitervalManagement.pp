dab050477962cdd44e67cfe97858e7cea78040fa
s3598797 <s3598797@student.rmit.edu.au>
2017-10-13 01:54:41 +1100

    attempt for runitervalManagement

--- scripts/runitervalManagement.pp
+++ scripts/runitervalManagement.pp
@@ -2,6 +2,6 @@
 class runitervalManagement {
 	augeas { 'interval':
 	context => '/files/etc/puppetlabs/puppet/puppet.conf',
-      changes => 'set runiterval = 1200', #check in for every 20 minutes / 3 times each hour
+      changes => 'set runiterval 1200', #check in for every 20 minutes / 3 times each hour
   }
 }
