3e8d2f8848a9cd5e41ed23370c9c0994a190b91e
s3598797 <s3598797@student.rmit.edu.au>
2017-10-13 01:41:54 +1100

    Duplicate declaration: Service[sshd] is already declared ; subscribe the httpd

--- scripts/subscribesManagement.pp
+++ scripts/subscribesManagement.pp
@@ -7,11 +7,7 @@ class subscribesManagement {
   # resources 
 
   #subscribe sshd 
-  service { 'sshd':
-    ensure  => 'running',
-    enable  => true,
-    require => Package['openssh-server'],
-  }
+
 
   # add a notify to the file resource
   file { '/etc/ssh/sshd_config':
@@ -19,8 +15,16 @@ class subscribesManagement {
     mode    => '0600',
     owner   => 'root',
     group   => 'root',
-    require => Package['openssh-server'],
     content => template('ssh/sshd_config.erb'),
+  }
+
+  file { '/etc/httpd/conf/httpd.conf':
+    notify  => Service['httpd'],  # this sets up the relationship
+    mode    => '0600',
+    owner   => 'root',
+    group   => 'root',
+    #require => Package['openssh-server'], #already installed
+    #content => template('ssh/sshd_config.erb'),
   }
 
 
