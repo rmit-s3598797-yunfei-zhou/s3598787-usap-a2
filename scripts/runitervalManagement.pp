3c6730f244bef1404c649b912b278e06f6df0af7
s3598797 <s3598797@student.rmit.edu.au>
2017-10-13 01:58:01 +1100

    run the cmd dirc

--- scripts/runitervalManagement.pp
+++ scripts/runitervalManagement.pp
@@ -1,7 +1,24 @@
 #Task 2
 class runitervalManagement {
-	augeas { 'interval':
-	context => '/files/etc/puppetlabs/puppet/puppet.conf',
-      changes => 'set runiterval 1200', #check in for every 20 minutes / 3 times each hour
+  # # Option 1:
+	# augeas { 'interval':
+	# context => '/files/etc/puppetlabs/puppet/puppet.conf',
+  #     changes => 'set runiterval 1200', #check in for every 20 minutes / 3 times each hour
+  # }
+   # resources
+
+  #Option 2
+  # schedule { 'everyday':
+  #   period => hourly,
+  #   repeat  => "3"
+  # }
+  # #This will cause resources to be applied every 20 minutes by default. (3 Times / hour )
+  # exec { "sudo /usr/local/bin/puppet agent -t":
+  #   schedule => 'everyday',
+  #    path  => ['/usr/bin', '/usr/sbin','/usr/local/bin'],
+  # }
+  exec { 'sudo /usr/local/bin/puppet agent --runinterval  1200':
+    path  => ['/usr/bin', '/usr/sbin','/usr/local/bin'],
   }
+  
 }
