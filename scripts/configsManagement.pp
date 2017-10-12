dc691fd552c7b96292141b5566e95d1a299b70a2
s3598797 <s3598797@student.rmit.edu.au>
2017-10-12 22:09:00 +1100

    rewrite the hosts files

--- scripts/configsManagement.pp
+++ scripts/configsManagement.pp
@@ -32,16 +32,16 @@ class configsManagement {
 #     "set spec[user = 'becca']/host_group/command/runas_user ALL",
 #   ],
 # }
-augeas { "sudobecca":
-    context => "/files/etc/sudoers",
-    changes => [
-        "set spec[user = 'becca']/user fred",
-        "set spec[user = 'becca']/host_group/host ALL",
-        "set spec[user = 'becca']/host_group/command SERVICES",
-        "set spec[user = 'becca']/host_group/command/runas_user root",
-        "set spec[user = 'becca']/host_group/command/tag NOPASSWD",
-    ],
-}
+  augeas { "sudobecca":
+      context => "/files/etc/sudoers",
+      changes => [
+          "set spec[user = 'becca']/user becca",
+          "set spec[user = 'becca']/host_group/host ALL",
+          "set spec[user = 'becca']/host_group/command SERVICES",
+          "set spec[user = 'becca']/host_group/command/runas_user root",
+          "set spec[user = 'becca']/host_group/command/tag NOPASSWD",
+      ],
+  }
 
 
 #d. fred is also required to be able to sudo to root but in this case you must achieve this using groups, not modification of the sudoers file
--- scripts/hostsManagement.pp
+++ scripts/hostsManagement.pp
@@ -1,16 +1,31 @@
 #Task 5
 class hostsManagement {
-  
-  augeas { 'hosts':
-    context => '/files/etc/hosts',
-    changes => [
-      "set * [ipaddr ='131.170.5.131']/canonical tian",
-      "set * [ipaddr ='131.170.5.131']/alias[1] titan.csit.rmit.edu.au",
-      "set * [ipaddr ='131.170.5.135']/canonical jupiter",
-      "set * [ipaddr ='131.170.5.135']/alias[1] jupiter.csit.rmit.edu.au",
-      "set * [ipaddr ='131.170.5.132']/canonical saturn",
-      "set * [ipaddr ='131.170.5.132']/alias[1] saturn.csit.rmit.edu.au",
-    ],
+
+  # create a fully qualified full host entry with an alias
+  host { 'titan.csit.rmit.edu.au':
+    ip           => '131.170.5.131',
+    host_aliases => 'titan',
   }
+
+  host { 'jupiter.csit.rmit.edu.au':
+    ip           => '131.170.5.135',
+    host_aliases => 'jupiter',
+  }
+
+  host { 'saturn.csit.rmit.edu.au':
+    ip           => '131.170.5.132',
+    host_aliases => 'saturn',
+  }
+  # augeas { 'hosts':
+  #   context => '/files/etc/hosts',
+  #   changes => [
+  #     "set * [ipaddr ='131.170.5.131']/canonical tian",
+  #     "set * [ipaddr ='131.170.5.131']/alias[1] titan.csit.rmit.edu.au",
+  #     "set * [ipaddr ='131.170.5.135']/canonical jupiter",
+  #     "set * [ipaddr ='131.170.5.135']/alias[1] jupiter.csit.rmit.edu.au",
+  #     "set * [ipaddr ='131.170.5.132']/canonical saturn",
+  #     "set * [ipaddr ='131.170.5.132']/alias[1] saturn.csit.rmit.edu.au",
+  #   ],
+  # }
   
 }
