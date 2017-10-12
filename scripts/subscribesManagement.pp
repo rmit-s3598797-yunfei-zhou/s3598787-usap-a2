0a8979fe2f8adad04544ed1f331cb82208669709
s3598797 <s3598797@student.rmit.edu.au>
2017-10-13 01:44:11 +1100

    Unix Permissions change

--- scripts/subscribesManagement.pp
+++ scripts/subscribesManagement.pp
@@ -12,7 +12,7 @@ class subscribesManagement {
   # add a notify to the file resource
   file { '/etc/ssh/sshd_config':
     notify  => Service['sshd'],  # this sets up the relationship
-    mode    => '0600',
+    mode    => '0644',
     owner   => 'root',
     group   => 'root',
     content => template('ssh/sshd_config.erb'),
@@ -20,7 +20,7 @@ class subscribesManagement {
 
   file { '/etc/httpd/conf/httpd.conf':
     notify  => Service['httpd'],  # this sets up the relationship
-    mode    => '0600',
+    mode    => '0644',
     owner   => 'root',
     group   => 'root',
     #require => Package['openssh-server'], #already installed
