0bd4afbcd5685a440055ae7e2ddcbf912ccbb138
s3598797 <s3598797@student.rmit.edu.au>
2017-10-12 12:57:48 +1100

    fix error for sudoers

--- scripts/configsManagement.pp
+++ scripts/configsManagement.pp
@@ -24,10 +24,10 @@ file{'/var/www/s3598797':
 augeas { 'sudoers':
   context => '/etc/sudoers',
   changes => [
-    'set spec[user = 'becca']/user becca',
-    'set spec[user = 'becca']/host_group/host ALL',
-    'set spec[user = 'becca']/host_group/command ALL',
-    'set spec[user = 'becca']/host_group/command/runas_user ALL',
+    "set spec[user = 'becca']/user becca",
+    "set spec[user = 'becca']/host_group/host ALL",
+    "set spec[user = 'becca']/host_group/command ALL",
+    "set spec[user = 'becca']/host_group/command/runas_user ALL",
   ],
 }
 #d. fred is also required to be able to sudo to root but in this case you must achieve this using groups, not modification of the sudoers file
